b64c5a888b8fb8eba0ee9f2c66b249c13907b659
d1mznpc <dimasakuq@gmail.com>
2025-06-11 09:16:21 +0700

    mengubah bootstrap menjadi tailwind

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,7 +22,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <!-- Delete Confirmation -->
